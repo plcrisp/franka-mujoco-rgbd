d3663773c81820314a392a57c47b1da979d1653e
plcrisp <pedrolcrisp@gmail.com>
2025-11-29 17:04:06 +0100

    ✨ png and npy depth

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import mujoco
 import mujoco.viewer
 import numpy as np
 import cv2
-import warnings
-
-warnings.filterwarnings("ignore")
 
 XML_PATH = "model/scene.xml"
 CAMERA_NAME = "end_effector_camera"
@@ -14,12 +11,26 @@ CAMERA_NAME = "end_effector_camera"
 m = mujoco.MjModel.from_xml_path(XML_PATH)
 d = mujoco.MjData(m)
 
-print("Loaded model with RGB-D camera.")
+print("RGB-D + PointCloud capture enabled.")
 
 renderer = mujoco.Renderer(m, 720, 1280)
 
 os.makedirs("photos", exist_ok=True)
+os.makedirs("photos/rgb", exist_ok=True)
+os.makedirs("photos/depth/png", exist_ok=True)
+os.makedirs("photos/depth/npy", exist_ok=True)
 last_photo = time.time()
+
+cam_id = m.camera(CAMERA_NAME).id
+fovy = m.camera(CAMERA_NAME).fovy 
+
+H, W = 720, 1280
+f = 0.5 * H / np.tan(0.5 * fovy * np.pi/180)    
+cx, cy = W/2, H/2                              
+
+print(f"Camera intrinsics: f={f}, cx={cx}, cy={cy}")
+
+
 
 with mujoco.viewer.launch_passive(m, d) as viewer:
     while viewer.is_running():
@@ -27,23 +38,29 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
         viewer.sync()
 
         renderer.update_scene(d, camera=CAMERA_NAME)
-        colour = renderer.render()
+        rgb = renderer.render()
 
         renderer.enable_depth_rendering()
         depth = renderer.render()
         renderer.disable_depth_rendering()
 
-        rgb_vis = cv2.cvtColor(colour, cv2.COLOR_RGB2BGR)
+        rgb_vis = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
         cv2.imshow("End Effector Camera (RGB)", rgb_vis)
         cv2.waitKey(1)
 
         if time.time() - last_photo >= 10:
             t = int(time.time())
 
-            cv2.imwrite(f"photos/{t}_rgb.png", rgb_vis)
+            # SAVE RGB
+            cv2.imwrite(f"photos/rgb/{t}.png", rgb_vis)
 
-            depth_vis = (depth * 255).astype(np.uint8)
-            cv2.imwrite(f"photos/{t}_depth.png", depth_vis)
+            # SAVE DEPTH VISUALIZATION
+            depth_vis = (depth / depth.max() * 255).astype(np.uint8)
+            cv2.imwrite(f"photos/depth/png/{t}.png", depth_vis)
 
-            print(f"Saved rgb_{t}.png and depth_{t}.png")
+            # SAVE DEPTH REAL (float32)
+            np.save(f"photos/depth/npy/{t}.npy", depth)
+
+            print(f"Saved RGB, Depth PNG and Depth NPY at time {t}")
+
             last_photo = time.time()
