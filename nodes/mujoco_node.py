4703ac17d15e45cd6564e3c78e79cfe4e3df6a9f
plcrisp <pedrolcrisp@gmail.com>
2025-12-09 22:15:22 +0100

    ✨ move the robot by publishing on ros node

--- nodes/mujoco_node.py
+++ nodes/mujoco_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
+from std_msgs.msg import Float64MultiArray
 import mujoco
 import mujoco.viewer
 import numpy as np
@@ -13,6 +14,7 @@ logging.getLogger('glfw').setLevel(logging.ERROR)
 XML_PATH = "../model/scene.xml"
 CAM_NAME = "end_effector_camera"
 WIDTH, HEIGHT = 1280, 720
+ACTUATOR_NAMES = ["actuator1","actuator2","actuator3","actuator4","actuator5","actuator6","actuator7"]
 
 class MujocoNode(Node):
     def __init__(self):
@@ -32,7 +34,18 @@ class MujocoNode(Node):
         self.pub_info = self.create_publisher(CameraInfo, '/camera/camera_info', 10)
         self.bridge = CvBridge()
 
-        # 4. Setup Camera Intrinsics
+        # 4. Setup ROS Subscriber (Control)
+        self.sub_control = self.create_subscription(
+            Float64MultiArray, 
+            '/joint_commands', 
+            self.control_callback, 
+            10
+        )
+
+        self.actuator_ids = [self.m.actuator(name).id for name in ACTUATOR_NAMES]
+        self.get_logger().info(f"Loaded Actuators: {self.actuator_ids}")
+
+        # 5. Setup Camera Intrinsics
         cam_id = mujoco.mj_name2id(self.m, mujoco.mjtObj.mjOBJ_CAMERA, CAM_NAME)
         fovy = self.m.cam_fovy[cam_id]
         f = 0.5 * HEIGHT / np.tan(0.5 * fovy * np.pi/180)
@@ -43,9 +56,17 @@ class MujocoNode(Node):
         self.cam_info.k = [f, 0., cx, 0., f, cy, 0., 0., 1.]
         self.cam_info.p = [f, 0., cx, 0., 0., f, cy, 0., 0., 0., 1., 0.]
 
-        # 5. Timer (30 Hz)
+        # 6. Timer (30 Hz)
         self.create_timer(1.0/30.0, self.loop)
         self.get_logger().info("MuJoCo Node Started.")
+
+    def control_callback(self, msg):
+        if len(msg.data) != 7:
+            self.get_logger().warn(f"Expected 7 commands, received {len(msg.data)}")
+            return
+        
+        for i, val in enumerate(msg.data):
+            self.d.ctrl[self.actuator_ids[i]] = val
 
     def loop(self):
         if not self.viewer.is_running():
