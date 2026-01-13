f085bad83d615744e9deaf5d2db9ad1b3bd6fab6
plcrisp <pedrolcrisp@gmail.com>
2026-01-13 23:45:20 +0100

    ✨ enable grasp logic and TF camera launch in run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -12,9 +12,12 @@ gnome-terminal --tab --title="MuJoCo Sim" -- bash -c "python3 nodes/simulation_n
 gnome-terminal --tab --title="YOLO Vision" -- bash -c "python3 nodes/object_segmentation_node.py; exec bash"
 
 # 4. Launch grasp logic
-# gnome-terminal --tab --title="Grasp Logic" -- bash -c "python3 grasp_detection_node.py; exec bash"
+gnome-terminal --tab --title="Grasp Logic" -- bash -c "python3 nodes/grasp_detection_node.py; exec bash"
+#gnome-terminal --tab --title="DIAGNOSTIC" -- bash -c "python3 nodes/diagnostic_node.py; exec bash"
 
 # 5. Launch commander
 gnome-terminal --tab --title="Commander" -- bash -c "python3 nodes/commander_node.py; exec bash"
+
+gnome-terminal --tab --title="TF Camera" -- bash -c "ros2 run tf2_ros static_transform_publisher --x 0.07 --y 0.0 --z 0.02 --roll 0 --pitch 0 --yaw 1.5707 --frame-id panda_hand --child-frame-id camera_optical_frame; exec bash"
 
 echo "System started!"
